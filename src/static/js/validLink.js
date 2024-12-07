1f4488c5687325ba2d8eee7f3aa5280f262ae838
Boyan <boyan@confest.im>
2024-12-08 00:19:10 +0100

    Update validation

--- src/static/js/validLink.js
+++ src/static/js/validLink.js
@@ -15,13 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // Function to validate URLs
     const isValidURL = (url) => {
         const pattern = new RegExp(
-            '^(https?:\\/\\/)?' + // Protocol (http or https)
-            '(([a-zA-Z0-9_-]+\\.)+[a-zA-Z]{2,6})' + // Domain name
-            '(\\/[a-zA-Z0-9@:%_\\+.~#?&//=]*)?$', // Path, query, or fragment
+            '^(https?:\\/\\/)' + // Protocol (http or https)
+            '(([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,})' + // Domain name
+            '(\\:[0-9]{1,5})?' + // Optional port number
+            '(\\/.*)?$', // Path, query string, or fragment
             'i'
         );
         return pattern.test(url);
     };
+    
 
     // Function to add https:// if missing
     const addHttpsIfMissing = (url) => {
